12e7cd869592d33844ff0fdeb6b8cc001105d376
Dennis <dengsauve@yahoo.com>
2016-09-05 15:39:26 -0700

    Code work and Improvements
    Working on the algorithm for choosing word combinations, and improving
    methods.

--- String in String/app.py
+++ String in String/app.py
@@ -1,15 +1,32 @@
-from string_tools import is_in_name, condense_string, get_dictionary
+from string_tools import is_word_in_target, condense_string, get_dictionary_list
+dictionary = get_dictionary_list('Dictionary/enableDict.txt')
+
 
 def main():
-    name_list = ("Donald Knuth", "Haskell Curry", "Dennis Sauve")
+    name_list = ("Ada Lovelace", "Haskell Curry", "Dennis Sauve")
     for name in name_list:
         name = condense_string(name)
         length, winner = 0, ""
-        dictionary = get_dictionary('Dictionary/enableDict.txt')
         for word in dictionary:
-            if name[0] == word[0] and length < len(word) <= len(name) and is_in_name(word, name):
+            if name[0] == word[0] and length < len(word) <= len(name) and is_word_in_target(word, name):
                 winner, length = word, len(word)
         print("For " + name + " the winner is: " + winner)
 
 
+def challenge():
+    name_list = ("Donald Knuth", "Alan Turing", "Claude Shannon"," Ada Lovelace", "Haskell Curry", "Dennis Sauve")
+    for name in name_list:
+        name = condense_string(name)
+        all_words = []
+        max_length = 0
+        for word in dictionary:
+            if name[0] == word[0] and len(word) <= len(name) and is_word_in_target(word, name):
+                all_words.append(word)
+                if len(word) > max_length:
+                    max_length = len(word)
+        print(all_words, max_length)
+
+
 main()
+# challenge()
+raise SystemExit
--- String in String/string_tools.py
+++ String in String/string_tools.py
@@ -1,19 +1,23 @@
 
 # Passed a word and a name, method checks to see if the letters of the word
 # appear, in order, in the name. If so, True is returned.
-def is_in_name(word, name):
-    counter, limit = 0, len(word)
-    for letter in name:
+def is_word_in_target(word, target, return_index=False):
+    counter, limit, end_index = 0, len(word), 0
+    for letter in target:
         if counter < limit and letter == word[counter]:
             counter += 1
-    return True if counter == limit else False
+            end_index = target.index(letter)
+    if return_index:
+        return True, end_index if counter == limit else False, end_index
+    else:
+        return True if counter == limit else False
 
-
+# Returns a string devoid of spaces and in all lowercase
 def condense_string(input_string):
     return input_string.lower().replace(' ','')
 
-
-def get_dictionary(filepath):
+# Returns
+def get_dictionary_list(filepath):
     with open(filepath) as f:
         dictionary = f.read().splitlines()
     return dictionary
