bbfcf2b626b316a8a819098bd2f54248916a721f
Dennis <dengsauve@yahoo.com>
2016-09-29 23:06:35 -0700

    Wrote scanning routine, listing everything

--- [2016-09-16] Challenge #283 [Hard] Guarding the Coast/main.py
+++ [2016-09-16] Challenge #283 [Hard] Guarding the Coast/main.py
@@ -1,11 +1,22 @@
 #!/bin/usr/dev python
 
-mapper = []
+side_length, max_length, mapper, coastline, plots = 0, 0, [], [], []# Create lists for map, coastline, and sets
 
 with open('sample.txt') as f:
-    mapper = f.read().splitlines()
+    side_length = int(f.readline())# side_length is the first value in text file
+    mapper = f.read().splitlines()# read rest of text file to the mapper
 
-for i in mapper:
-    print(i)
+for y in mapper:
+    if len(y) > max_length:
+        max_length = len(y)# Value to be used later in creating sets
+    for x in y:
+        if x == "*":
+            coastline.append((index[y], index[x]))# Co-ords are (row, column) top down, left right.
 
-input("hi")
+for y in mapper:
+    for x in range(max_length - side_length):
+        subset = []
+        for yy in range(side_length):
+            for xx in range(side_length):
+                subset.append((index(y) + yy, x + xx))
+        plots.append(subset)
