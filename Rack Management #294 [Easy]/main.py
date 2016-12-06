f956f6f52fce8fad3ae79aba0cfb693c4ab82eaf
Dennis <dengsauve@yahoo.com>
2016-12-06 14:20:54 -0800

    Completed Challenge + Bonus 1 and 2
    Attempted #3, didn't work completely. Ran out of time with finals and Advent of Code going on.

--- Rack Management #294 [Easy]/main.py
+++ Rack Management #294 [Easy]/main.py
@@ -1,3 +1,11 @@
+with open("enable.txt", "r") as f:
+    dictionary = f.read().splitlines()
+with open("scoring.txt", "r") as f:
+    data, scoring = f.read().splitlines(), {}
+    for block in data:
+        scoring[block.split()[0]] = int(block.split()[1])
+
+
 def scrabble(super_string, sub_string):
     x, y = list(sub_string), list(super_string)
     for i in x:
@@ -12,14 +20,37 @@ def scrabble(super_string, sub_string):
 
 
 def longest(super_string):
-    print("Solving for:", super_string)
-    dictionary = []
-    with open("enable.txt", "r") as f:
-        dictionary = f.read().splitlines()
     for word in sorted(dictionary, key=lambda s: len(s), reverse=True):
-        print(word)
-        if len(word) <= 20 and scrabble(super_string, dictionary):
-            return word
+        if len(word) <= len(super_string):
+            if scrabble(super_string, word):
+                return word
+
+
+def score(word, word_sum=0):
+    for i in word:
+        word_sum += scoring[i]
+    return word_sum
+
+
+def get_scored_letters(super_string, word):
+    letters, x, y = "", list(word), list(super_string)
+    for i in x:
+        try:
+            y.remove(i)
+            letters += i
+        except ValueError:
+            pass
+    return letters
+
+
+def highest(super_string):
+    return_word = ""
+    for word in dictionary:
+        if scrabble(super_string, word):
+            scored_letters = get_scored_letters(super_string, word)
+            if score(scored_letters) > score(return_word):
+                return_word = word
+    return return_word
 
 
 print(scrabble("ladilmy", "daily"))  # True
@@ -37,3 +68,9 @@ print(longest("uruqrnytrois"))  # "turquois"
 print(longest("rryqeiaegicgeo??"))  # "greengrocery"
 print(longest("udosjanyuiuebr??"))  # "subordinately"
 print(longest("vaakojeaietg????????"))  # "ovolactovegetarian"
+
+print(highest("dcthoyueorza"))  # "zydeco"
+print(highest("uruqrnytrois"))  # "squinty"
+print(highest("rryqeiaegicgeo??"))  # "reacquiring"
+print(highest("udosjanyuiuebr??"))  # "jaybirds"
+print(highest("vaakojeaietg????????"))  # "straightjacketed"
