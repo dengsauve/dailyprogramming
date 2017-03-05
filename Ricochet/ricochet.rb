be1c62c57b98082dfa9099c31c2edcb7bafce16e
Dennis <dengsauve@yahoo.com>
2017-03-05 06:55:29 -0800

    working on help menu

--- Ricochet/ricochet.rb
+++ Ricochet/ricochet.rb
@@ -1,4 +1,34 @@
-# Creates a 2D array of spaces
+############################################################
+#
+#  Name:         Dennis Sauve
+#  Date:         03/05/17
+#  Assignment:   dailyprogrammer challenge - Ricochet
+#  Class:        -
+#  Description:  A program that determines where a particle
+#                traveling at 45 degrees will land.
+#
+#  Desired CLI:  ricochet.rb #runs with dummmy data
+#                ricochet.rb h w v as numbers
+#                ricochet.rb h w v m as numbers with m indicating no mapping
+#
+############################################################
+
+def print_help
+    system('clear') or system('cls')
+    puts "\tHELP - RICOCHET\n
+    \tricochet.rb - Cue ball, Corner Pocket (but which corner)\n
+    \tUSAGE: height width velocity {OPT: -p particle_height particle_width; -m}\n
+    \t-p: Specifies that you want a custom particle size (other than 1x1).
+    \t-m: Specifies that you want the conclusion quickly, and no animation.\n
+    \tINPUT:    height, width, velocity
+    \theight:   the height of the table.
+    \twidth:    the width of the table.
+    \tvelocity: the velocity at which the particle is traveling.\n
+    \tOUTPUT:   Corner, Number of Bounces, Time taken\n
+    \tSUGGESTIONS: If you have a low velocity value (i.e. <10), I suggest -m as the animation could be lengthy."
+    exit
+end
+
 def map_maker(h, w, map=[])
     (1..h).each { map << [] }
     map.each { |row| (1..w).each { row << ' ' } }
@@ -6,17 +36,18 @@ def map_maker(h, w, map=[])
 end
 
 def print_map(map)
-    sleep (1.0/15.0)
+    sleep (1.0/ARGV[2].to_i)
     system('clear') or system('cls')
     puts '#'*(map[0].size+2)
     map.each { |row| print '#'; row.each { |col| print col}; puts '#' }
     puts '#'*(map[0].size+2)
 end
 
-input1 = '8 3 1'.split.map(&:to_i)
-input2 = '15 4 2'.split.map(&:to_i)
-
-h, w, v = input2
+(ARGV.include? '-h') ? print_help : puts
+(ARGV.include? '--help') ? print_help : puts
+(ARGV.include? '-m') ? mapping=false : mapping=true
+(ARGV.include? '-p') ? custom_p=true : custom_p=false
+h, w, v = ARGV.map(&:to_i)
 bounce, moves = 0, 0
 corners, map = {0=>'UL', (w-1)=>'UR',((h-1)+(w-1))=>'LR', (h-1)=>'LL'}, map_maker(h, w)
 particle, x, y = [0,0], 1, 1
@@ -33,7 +64,7 @@ map[particle[0]][particle[1]] = 'O'
 moves += 1
 
 while particle != [0,0] && particle!= [0,w-1] && particle != [h-1,w-1] && particle != [h-1,0]
-    print_map(map)
+    print_map(map) if mapping
     map[particle[0]][particle[1]] = ' '
     if particle[0] > 0 && particle[0] < h-1
         particle[0] += y
@@ -54,5 +85,5 @@ while particle != [0,0] && particle!= [0,w-1] && particle != [h-1,w-1] && partic
 end
 
 print_map(map)
-time = moves * v
+time = moves.to_f / v.to_f
 puts corners[particle[0]+particle[1]] + ' ' + bounce.to_s + ' ' + time.to_s
