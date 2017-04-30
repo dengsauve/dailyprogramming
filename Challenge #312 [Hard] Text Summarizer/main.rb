ce4bcdd4870c4c50a4c36fb4548a227015ee47fc
Dennis <dengsauve@yahoo.com>
2017-04-30 07:17:34 -0700

    Finished Text-Summary Challenge

--- Challenge #312 [Hard] Text Summarizer/main.rb
+++ Challenge #312 [Hard] Text Summarizer/main.rb
@@ -3,7 +3,6 @@
 # Challenge: Write an implementation of a text summarizer that can take a block of text (e.g. a paragraph),
 # and emit a one or two sentence summarization of it.
 
-
 # Read in Stop list
 stop_file, stop_list = File.open('stop.txt','r+'), []
 stops = stop_file.readlines
@@ -29,32 +28,24 @@ text.split('').each_with_index do |character, index|
     sentence += character
   end
 end
-puts sentences
-
 
 # score sentences initially at 0.0, and -1 point for every stop word.
-
 # divide float score by sentence length
+sentence_score = {}
+sentences.each do |sentence|
+  sentence_score[sentence] = 0.0
+end
+
+sentences.each do |sentence|
+  score = 0
+  stop_list.each do |stop|
+    score += 1 if sentence.include?(stop)
+  end
+  sentence_score[sentence] = (sentence_score[sentence] - score)/sentence.length
+end
 
 # Take the two highest scoring sentences.
+description = sentence_score.sort_by { |k, v| v}.reverse[0][0].strip + '.'
+description += sentence_score.sort_by { |k, v| v}.reverse[1][0] + '.'
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#end
+puts description
