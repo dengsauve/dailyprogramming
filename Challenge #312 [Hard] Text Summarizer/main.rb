4372831b3727f7408432d41fee3f28157cfdfce1
Dennis <dengsauve@yahoo.com>
2017-04-30 06:12:48 -0700

    Imported Support Files
    Scaffolded Approach to solution

---
+++ Challenge #312 [Hard] Text Summarizer/main.rb
@@ -0,0 +1,51 @@
+# den510
+# DailyProgrammer
+# Challenge: Write an implementation of a text summarizer that can take a block of text (e.g. a paragraph),
+# and emit a one or two sentence summarization of it.
+
+
+# Read in Stop list
+stop_file = File.open('stop.txt','r+')
+stops = stop_file.readlines
+stops.each do |line|
+  
+end
+
+
+stop_file.close
+
+
+
+
+
+# Read in paragraph.text
+
+
+
+
+# score sentences initially at 0.0, and -1 point for every stop word.
+
+# divide float score by sentence length
+
+# Take the two highest scoring sentences.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#end
