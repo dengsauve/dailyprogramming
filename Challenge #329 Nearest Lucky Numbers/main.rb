b20aa617ac45fc571fad88f39be05f9445fc1d8e
Dennis <dengsauve@yahoo.com>
2017-09-03 19:39:34 -0700

    Preprogrammed early odds filter
    Set to filter any number where n+1 % 6 == 0. This 1/2'd my time when
    solving for 1,000,000 to about 11.9 seconds.

--- Challenge #329 Nearest Lucky Numbers/main.rb
+++ Challenge #329 Nearest Lucky Numbers/main.rb
@@ -3,11 +3,46 @@ def is_lucky(num, nums)
 end
 
 
-# ini vars: ll_num is 'latest lucky number'
-start = Time.now
-index = 0
-ll_num = 1
+def create_set(input)
+  index = 1
+  ll_num = 1
+  # Generate array to 2*n
+  set = (1..(input + 20).to_i).to_a
 
+  # Filter array of evens
+  set.delete_if {|number| number.even?}
+
+  # Filter array of every third odd
+  set.delete_if {|number| (number + 1) % 6 == 0}
+
+  # Filter array using Lucky Number Sieve
+  until ll_num >= (input / 2) or ll_num > set.length
+    # Increment to next sieve value
+    index += 1
+    # Set increment
+    ll_num = set[index]
+    limit = (set.length - 1) / ll_num * ll_num
+    ( -limit..-(ll_num - 1) ).step(ll_num) do |number|
+      set.delete_at(-number - 1)
+    end
+  end
+  return set
+end
+
+# Find lucky number(s)
+def find_lucky_numbers(input)
+  set = create_set(input)
+  unless is_lucky(input, set)
+    min = input
+    until is_lucky(min, set)
+      min -= 1
+    end
+    max = set[set.index(min) + 1]
+    return "#{min} < #{input} < #{max}"
+  else
+    return "#{input} is a lucky number!"
+  end
+end
 
 # get integer
 if ARGV.empty?
@@ -17,45 +52,8 @@ else
   input = ARGV[0].to_i
 end
 
+start = Time.now
 
-# Generate array to 2*n
-set = (1..(input*1.1).to_i).to_a
-
-
-# Filter array of evens
-set.delete_if {|number| number.even?}
-
-
-# Filter array using Lucky Number Sieve
-until ll_num >= (input / 2) or ll_num > set.length
-  # Increment to next sieve value
-  index += 1
-
-  # Set increment
-  ll_num = set[index]
-
-  limit = (set.length - 1) / ll_num * ll_num
-  ( -limit..-(ll_num - 1) ).step(ll_num) do |number|
-    set.delete_at(-number - 1)
-  end
-end
-
-
-# Find lucky number(s)
-unless is_lucky(input, set)
-  min = input
-  until is_lucky(min, set)
-    min -= 1
-  end
-  max = set[set.index(min) + 1]
-  puts "#{min} < #{input} < #{max}"
-else
-  puts "#{input} is a lucky number!"
-end
+puts find_lucky_numbers(input)
 
 puts "Time elapsed: #{Time.now - start} second(s)"
-
-
-set.each do |i|
-  puts set[set.index(i) - 1] 
-end
