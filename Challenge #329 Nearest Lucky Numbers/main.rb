8d3fa33bacfe5d90ffc86edefa83307d20ce7e2e
Dennis <dengsauve@yahoo.com>
2017-09-03 18:13:36 -0700

    Shaved some time by step iteration

--- Challenge #329 Nearest Lucky Numbers/main.rb
+++ Challenge #329 Nearest Lucky Numbers/main.rb
@@ -2,10 +2,12 @@ def is_lucky(num, nums)
   return nums.include?(num)
 end
 
-# ini vars
+
+# ini vars: ll_num is 'latest lucky number'
 start = Time.now
 index = 0
 ll_num = 1
+
 
 # get integer
 if ARGV.empty?
@@ -15,35 +17,37 @@ else
   input = ARGV[0].to_i
 end
 
+
 # Generate array to 2*n
-set = (1..(input*1.5).to_i).to_a
+set = (1..(input*1.1).to_i).to_a
+
 
 # Filter array of evens
-# set.delete_if {|number| (set.index(number) + 1) % 2 == 0}
 set.delete_if {|number| number.even?}
+
 
 # Filter array using Lucky Number Sieve
 until ll_num >= (input / 2) or ll_num > set.length
+  # Increment to next sieve value
   index += 1
+
+  # Set increment
   ll_num = set[index]
-  #puts ll_num
-  nd = []
-  (1..(set.length - 1)).step(ll_num) do |number|
-    nd << (number - 1) if number > index
+  
+  limit = (set.length - 1) / ll_num * ll_num
+  ( -limit..-(ll_num - 1) ).step(ll_num) do |number|
+    set.delete_at(-number - 1)
   end
-  nd.reverse!
-  puts nd.inspect
-  nd.each do |i|
-    set.delete_at(i - 1)
-  end
-  #set.delete_if {|number| (set.index(number) + 1) % ll_num == 0}
-  #puts set.inspect
-  #sleep(0.5)
 end
+
 
 # Find lucky number(s)
 unless is_lucky(input, set)
-  min = set.min_by { |x| (x - input).abs }
+  min = input
+  until is_lucky(min, set)
+    min -= 1
+  end
+  #min = set.min_by { |x| (x - input).abs }
   max = set[set.index(min) + 1]
   puts "#{min} < #{input} < #{max}"
 else
