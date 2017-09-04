24f12ed8f50317bbf9b6e2013421b52f21f6ac76
Dennis <dengsauve@yahoo.com>
2017-09-03 21:32:20 -0700

    Beginning work on C# solution

---
+++ Challenge #329 Nearest Lucky Numbers/C-Sharp/LuckyCompiled/LuckyCompiled/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuckyCompiled
+{
+    class Program
+    {
+        static bool isLucky(int num, List<int> nums)
+        {
+            return nums.IndexOf(num) != -1;
+        }
+
+        static List<int> createSet(int input)
+        {
+            int index = 1;
+            int ll_num = 1;
+
+            List<int> set = Enumerable.Range(1, input).ToList();
+
+            set.RemoveAll(n => n % 2 == 0);
+            set.RemoveAll(n => (n + 1) % 6 == 0);
+
+            while(ll_num < set.Count)
+            {
+
+            }
+
+            return set;
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+            List<int> nums = createSet(50);
+            int num = 7;
+            Console.WriteLine(isLucky(num, nums));
+        }
+    }
+}
